f665784d2be9e0c7408f4e6a4be267eafacea516
Doszka813 <lewillas@gmail.com>
2019-04-11 11:26:31 +0200

    init commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,10 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-var config = {
-
-};
+import '../../config';
+var config = config.firebaseConfig;
 
 firebase.initializeApp(config);
 firebase.firestore();
