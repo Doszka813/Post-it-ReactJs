a9daeba276ac1924137a5c08228c2d868aa36b96
Doszka813 <lewillas@gmail.com>
2019-04-11 11:42:55 +0200

    init commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,8 +1,15 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-import '../../config';
-var config = config.firebaseConfig;
+
+var config = {
+  apiKey: "AIzaSyAbCNTz2QCc9uYoLFVyhAkHENQ7WUK0oqU",
+  authDomain: "postit-a38b6.firebaseapp.com",
+  databaseURL: "https://postit-a38b6.firebaseio.com",
+  projectId: "postit-a38b6",
+  storageBucket: "postit-a38b6.appspot.com",
+  messagingSenderId: "514224469915"
+};
 
 firebase.initializeApp(config);
 firebase.firestore();
